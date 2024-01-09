4f988a4c3eddb4fc10d68364c28f338732906b3e
Ryan Freed <rya12533@esri.com>
2024-01-09 17:00:57 -0500

    fixed syntax errors, added docstring and removed folder functionality

--- get_participating_items.py
+++ get_participating_items.py
@@ -1,35 +1,20 @@
 from arcgis.gis import GIS
 from arcgis.gis import Item
-from datetime import datetime
 
 import pandas as pd
-import json
-from getpass import getpass
-
-def flatten_data(y):
-    out = {}
-
-    def flatten(x, name=''):
-        if type(x) is dict:
-            for a in x:
-                flatten(x[a], name + a + '_')
-        elif type(x) is list:
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + '_')
-                i += 1
-        else:
-            out[name[:-1]] = x
-
-    flatten(y)
-    return out
 
 def get_participating_items(item: Item, gis: GIS, user: str) -> pd.DataFrame:
     """
-    TODO
-    """
+    Generates a Pandas dataframe of content participating in item. 
     
-    folders = gis.users.search(user)[0].folders
+    Arguments:
+        item (Item): Item which will be searched through for participating content.
+        gis (GIS): Active GIS of the item.
+        user (User): Owner of the content
+
+    Returns:
+        results (Dataframe): Pandas Dataframe of participating content within item.
+    """
     
     def flatten_data(y):
         out = {}
@@ -52,20 +37,17 @@ def get_participating_items(item: Item, gis: GIS, user: str) -> pd.DataFrame:
     related_items = [] # list of items retrieved from JSON
     data = [] # pandas data
     
-    item_json = item.get_data()
+    item_json = item.get_data(try_json=True)
     flat_keys = flatten_data(item_json)
-    for k, v in flat_keys.items():
-        related_items.append(gis.content.get(v))
     
+    for k, v in flat_keys.items():
+        if "itemId" in k:
+            related_itm = gis.content.get(v)
+            related_items.append(related_itm)
+
     for itm in related_items:
-        # get folders
-        for fldr in folders:
-            if fldr['id'] == item.ownerFolder:
-                fldr_name = fldr['title']
-            else:
-                fldr_name = 'root'
         
-        d = {"Item Name": itm.title, "Author": item.owner, "Item ID": itm.id, "Type": itm.type, "Tags": item.tags, "Folder": fldr_name}
+        d = {"Item Name": itm.title, "Author": item.owner, "Item ID": itm.id, "Type": itm.type, "Tags": item.tags}
         data.append(d)
         
     result = pd.DataFrame(data)
